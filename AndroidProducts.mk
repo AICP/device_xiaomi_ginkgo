93fb8e5788183e3994bafe8868860acf27371e0e
Edgars-Cirulis <edgarsjc5@gmail.com>
2020-09-17 21:52:28 +0300

    ginkgo: CarbonROM Bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 CarbonROM
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/carbon_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    carbon_ginkgo-user \
+    carbon_ginkgo-userdebug \
+    carbon_ginkgo-eng
---
+++ carbon_ginkgo.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2020 CarbonROM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from ginkgo device
+$(call inherit-product, device/xiaomi/ginkgo/device.mk)
+
+ # Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common.mk) 
+
+# MIUICamera stuff.
+$(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
+
+
+# Device identifier
+PRODUCT_NAME := carbon_ginkgo
+PRODUCT_DEVICE := ginkgo
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 8
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Fingerprint
+BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
