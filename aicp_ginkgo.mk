40cf20ee65bb9a1ab5cff3561b1eb0869e7f0d66
Kitsunejasutin <justineryandeguia@yahoo.com>
2021-05-28 09:22:21 +0800

    Set Target Screen Height and Width

--- aicp_ginkgo.mk
+++ aicp_ginkgo.mk
@@ -24,6 +24,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Inherit some common AICP stuff
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2340
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_PIXEL_CHARGER := true
