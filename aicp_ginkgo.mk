90c36eee0e7f809a051926dfba56e6782b0d37b4
Kitsunejasutin <justineryandeguia@yahoo.com>
2021-06-20 06:08:40 +0800

    ginkgo: Add AICP maintainer info
    Change-Id: I59e3504780bb5223952a3bb5654f5787b68eaddd

--- aicp_ginkgo.mk
+++ aicp_ginkgo.mk
@@ -42,3 +42,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_OVERRIDE_INFO := true
 PRODUCT_OVERRIDE_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
 
+#AICP Device Maintainer
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Justine Ryan De Guia (Tokatsu)"
